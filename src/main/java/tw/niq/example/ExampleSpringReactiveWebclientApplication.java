59bfb7769b4ab44c02fe12a49c2eb83223a04033
Nick <nick.chen@cmp.com.tw>
2023-08-31 16:20:50 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/ExampleSpringReactiveWebclientApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringReactiveWebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringReactiveWebclientApplication.class, args);
+	}
+
+}
