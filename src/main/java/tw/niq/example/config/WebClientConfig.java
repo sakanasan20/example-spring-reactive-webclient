5c26ff77160f9d25b0dfaefd565b9737761890f8
Nick <nick.chen@cmp.com.tw>
2023-09-01 16:05:12 +0800

    Add OAuth2 client support

--- src/main/java/tw/niq/example/config/WebClientConfig.java
+++ src/main/java/tw/niq/example/config/WebClientConfig.java
@@ -3,20 +3,33 @@ package tw.niq.example.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient.Builder;
 
 @Configuration
 public class WebClientConfig implements WebClientCustomizer {
 
 	private final String baseUrl;
+	private final ReactiveOAuth2AuthorizedClientManager authorizedClientManager;
 
-	public WebClientConfig(@Value("${webclient.baseUrl}") String baseUrl) {
+	public WebClientConfig(
+			@Value("${webclient.baseUrl}") String baseUrl, 
+			ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
+		
 		this.baseUrl = baseUrl;
+		this.authorizedClientManager = authorizedClientManager;
 	}
 
 	@Override
 	public void customize(Builder webClientBuilder) {
-		webClientBuilder.baseUrl(baseUrl);
+		
+		ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = 
+				new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+		
+		oauth.setDefaultClientRegistrationId("oidcclient");
+		
+		webClientBuilder.filter(oauth).baseUrl(baseUrl);
 	}
 	
 }
